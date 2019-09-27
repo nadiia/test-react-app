b3467607e151e9dd1a1858381d12b0ad0dcda6e1
Nadiia Dmytrenko <nadiia@uber.com>
2019-09-27 13:57:44 -0700

    pin baseui beta and webpack 4.32.0

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -173,7 +173,7 @@ module.exports = function(webpackEnv) {
         ? 'static/js/[name].[contenthash:8].js'
         : isEnvDevelopment && 'static/js/bundle.js',
       // TODO: remove this when upgrading to webpack 5
-      futureEmitAssets: true,
+      // futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
       chunkFilename: isEnvProduction
         ? 'static/js/[name].[contenthash:8].chunk.js'
